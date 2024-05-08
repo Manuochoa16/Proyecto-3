90defc59db8c4c4ec21f8d51834996c653bab3ca
manuochoa16 <nicomanu2316@gmail.com>
2024-05-08 16:01:20 -0300

    HW clase 7

---
+++ front/vite-project/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <div>
+            <div>
+                <span>HOME</span><br></br>
+                <span>REGISTRARSE</span><br></br>
+                <span>INGRESAR</span><br></br>
+                <span>CONTACTO</span>
+            </div>
+        </div>
+    )
+};
+
+export default NavBar;
